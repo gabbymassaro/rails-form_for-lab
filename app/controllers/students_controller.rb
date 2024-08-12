915d7424af88c4155e5ab87a23ceeccf3c538a97
Gabriella Massaro <gmassaro807@gmail.com>
2024-08-12 14:58:07 -0400

    All Student Tests Passing

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,20 +1,31 @@
 class StudentsController < ApplicationController
 
-  def create
-    student = Student.create(student_params)
-    redirect_to student_path(student)
-  end
-
   def index
     @students = Student.all
-  end
-
-  def new
   end
 
   def show
     @student = Student.find(params[:id])
   end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    student = Student.create(student_params)
+    redirect_to student_path(student)
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+	  @student = Student.find(params[:id])
+    @student.update(student_params)
+    redirect_to student_path(@student)
+	end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :students, only: [:index, :show, :new, :create, :edit, :update]
   resources :school_classes, only: [:index, :show, :new, :create, :edit, :update]
   patch 'school_classes/:id', to: 'school_classes#update'
+  patch 'students/:id', to: 'students#update'
+
 end
